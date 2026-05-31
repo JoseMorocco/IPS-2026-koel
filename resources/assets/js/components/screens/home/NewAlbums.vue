8ddad86dca41d40f69306ccf6d6bf7f60111ea7b
unknown <jmoroccosa@unsa.edu.pe>
2026-05-30 20:20:09 -0500

    Test: Se realiza un segundo Test de CD

--- resources/assets/js/components/screens/home/NewAlbums.vue
+++ resources/assets/js/components/screens/home/NewAlbums.vue
@@ -1,6 +1,6 @@
 <template>
   <HomeScreenBlock>
-    <template #header>Latest Albums - CI Deploy Check</template>
+    <template #header>Latest Albums - Este es un Segundo Test De CD</template>
     <ol
       v-if="loading"
       class="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-3"
